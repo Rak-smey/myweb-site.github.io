9b29e8b9622a23ed58313bceefe7445589ab85f2
Rak-smey <135052666+Rak-smey@users.noreply.github.com>
2023-06-07 01:32:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -19,3 +19,36 @@ function calculate() {
     result.value = 'Error';
   }
 }
+const heading = document.querySelector("h1");
+
+// define an array of styles to cycle through
+const styles = [
+  { color: "red", fontWeight: "normal", fontStyle: "normal" },
+  { color: "blue", fontWeight: "bold", fontStyle: "italic" },
+  { color: "green", fontWeight: "normal", fontStyle: "italic" },
+  { color: "purple", fontWeight: "bold", fontStyle: "normal" }
+];
+
+// set an initial index for the style array
+let styleIndex = 0;
+
+// define the animation function
+function animateStyle() {
+  // update the styles of the heading
+  const currentStyle = styles[styleIndex];
+  heading.style.color = currentStyle.color;
+  heading.style.fontWeight = currentStyle.fontWeight;
+  heading.style.fontStyle = currentStyle.fontStyle;
+  
+  // increment the style index or reset it if we've reached the end of the array
+  if (styleIndex < styles.length - 1) {
+    styleIndex++;
+  } else {
+    styleIndex = 0;
+  }
+}
+
+// call the animation function every 500 milliseconds
+setInterval(animateStyle, 500);
+
+
